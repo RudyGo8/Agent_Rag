70d65441cf11cef496c50f54656f282fb098e32d
ryder__code <1579101153@qq.com>
2026-03-18 13:53:06 +0800

    add RAG_vector_stores

--- RAG项目案例/app_file_uploader.py
+++ RAG项目案例/app_file_uploader.py
@@ -3,10 +3,12 @@
 @Author: GeChao
 @File: app_file_uploader.py
 '''
+import time
 
 # 基于Streamlit完成WEB网页上传服务
 
 import streamlit as st
+from knowledge_base import KnowledgeBaseService
 
 st.title("知识库")
 
@@ -17,6 +19,12 @@ uploader_file = st.file_uploader(
     accept_multiple_files=False,  # False表示仅支持一个文件的上传
 )
 
+service = KnowledgeBaseService()
+
+# session_state就是一个字典
+if "service" not in st.session_state:
+    st.session_state["service"] = KnowledgeBaseService()
+
 if uploader_file is not None:
     file_name = uploader_file.name
     file_type = uploader_file.type
@@ -26,4 +34,7 @@ if uploader_file is not None:
     st.write(f"格式: {file_type} | 大小: {file_size:.2f} KB")
 
     text = uploader_file.getvalue().decode('utf-8')
-    st.write(text)
+    with st.spinner("载入知识库中。。。"):
+        time.sleep(1)
+        result = st.session_state["service"].upload_by_str(text, file_name)
+        st.write(result)
--- RAG项目案例/knowledge_base.py
+++ RAG项目案例/knowledge_base.py
@@ -10,6 +10,11 @@
 import os
 import config_data as config
 import hashlib
+from langchain_chroma import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import DashScopeEmbeddings
+from datetime import datetime
+
 
 def check_md5(md5_str: str):
     """
@@ -47,19 +52,49 @@ def get_string_md5(input_str: str, encoding: str = 'utf-8'):
     md5_hex = md5_obj.hexdigest()
     return md5_hex
 
+
 class KnowledgeBaseService(object):
     def __init__(self):
-        self.chroma = None  # 向量存储的实例 Chroma向量库对象
-        self.spliter = None  # 文本分割器的对象
+        # 如果文件不存在则创建，如果存在则跳过
+        os.makedirs(config.persist_directory, exist_ok=True)
+        self.chroma = Chroma(
+            collection_name=config.collection_name,  # 数据库的表名
+            embedding_function=DashScopeEmbeddings(model="text-embedding-v4"),
+            persist_directory=config.persist_directory,  # 数据库本地存储文件夹
+        )  # 向量存储的实例 Chroma向量库对象
+        self.spliter = RecursiveCharacterTextSplitter(
+            chunk_size=config.chunk_size,  # 分割后的文本段最大长度
+            chunk_overlap=config.chunk_overlap,  # 连续文本段之间的字符重叠数量
+            separators=config.separators,  # 自然段落划分的符号
+            length_function=len  # 统计字符的依据函数
+        )  # 文本分割器的对象
 
     def upload_by_str(self, data, filename):
         """将传入的字符串，进行向量化，存入向量数据库中"""
-        pass
+        md5_hex = get_string_md5(data)
+        if check_md5(md5_hex):
+            return "[跳过]内容已经存在知识库中"
+        if len(data) > config.max_split_char_number:
+            knowledge_chunks: list[str] = self.spliter.split_text(data)
+        else:
+            knowledge_chunks = [data]
+
+        metadata = {
+            "source": filename,
+            "created_at": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "operator": "Rudy"
+        }
+
+        self.chroma.add_texts(  # 内容就加载到向量库中了
+            knowledge_chunks,
+            metadatas=[metadata for _ in knowledge_chunks],
+
+        )
+        save_md5(md5_hex)
+        return "[成功]内容已经成功载入向量库"
+
 
 if __name__ == '__main__':
-    r1 = get_string_md5("AI开发1")
-    r2 = get_string_md5("AI开发1")
-    r3 = get_string_md5("AI开发3")
-    save_md5("747211adaeb43406bd69aa62ac04e017")
-
-    print(check_md5("747211adaeb43406bd69aa62ac04e017"))
+    service = KnowledgeBaseService()
+    r = service.upload_by_str("AI开发2", "前景很好")
+    print(r)
